55ef35dcc1a07666fc31f9f21f6e7ab69c2fb464
Guilherme Kono <briares65@gmail.com>
2023-07-27 16:11:30 -0300

    CR com jwt, autenticação e validação

--- app.js
+++ app.js
@@ -19,6 +19,46 @@ app.get('/', (req, res) => {
     res.status(200).json({ msg: 'bem vindo a nossa API'})
 })
 
+//Private Route
+
+app.get("/user/:id", checkToken, async (req, res) => {
+
+    const id = req.params.id
+
+    // check if user exists
+    const user = await User.findById(id, '-password')
+
+    if(!user){
+        return res.status(404).json({msg: 'Usuário não encontrado'})
+    }
+
+    res.status(200).json({ user })
+})
+
+function checkToken(req, res, next){//função que verifica o token, isso é um middlware com acesso à requisição, à resposta e ao parametro next, que indica que ta tudo certo e é pra continuar, ou indica que tem um erro e cai fora
+
+    const authHeader = req.headers['authorization'] //acesso ao token em si. É necessário extrair a parte útil do token, pois ele vem como 'Bearer &*uashduhasd', sendo que só a parte depois do bearer é o token em si
+    const token = authHeader && authHeader.split(" ")[1]
+    console.log(token)
+
+    if(!token){
+        return res.status(401).json({msg: "Acesso negado pois usuário não tem token"})
+    }
+
+    try {
+
+        const secret = process.env.SECRET
+
+        jwt.verify(token, secret) //verifica se token existe?
+
+        next()
+
+    } catch(error){
+        res.status(400).json({msg:"Token inválido"})
+    }
+
+}
+
 // Register User
 app.post('/auth/register', async(req, res) => {
 
@@ -96,6 +136,28 @@ app.post("/auth/login", async (req, res) => {
 
     if(!checkPassword){
         return res.status(422).json({msg: 'Senha inválida'})
+    }
+
+    try {
+
+        const secret = process.env.secret
+
+        const token = jwt.sign({
+
+            id: user._id,
+
+            },
+            secret,
+        )
+
+        res.status(200).json({ msg: 'Autenticação realizada com sucesso', token})
+
+    } catch(err){
+        console.log(error)
+
+        res.status(500).json({
+            msg: 'Aconteceu um erro no servidor, tente novamente mais tarde!',
+        })
     }
 })
 
